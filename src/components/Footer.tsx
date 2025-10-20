fccb01e8ce9c089c4d752de096686951e30d902c
Rahul Pujari <pujarirahulpandu@gmail.com>
2025-10-20 19:20:18 +0530

    formspree id

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
           <div>
             <div className="flex items-center gap-2 mb-4">
               <ImageWithFallback 
-                src="/ecell-logo.svg"
+                src="icon.png"
                 alt="E-Cell IARE Logo"
                 className="w-10 h-10 object-contain"
               />
